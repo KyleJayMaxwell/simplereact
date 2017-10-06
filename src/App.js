749b4159f736fb4195c3568ab7015af4c07e11c9
Kyle Maxwell <kylejaymaxwell@gmail.com>
2017-10-05 22:40:01 -0400

    used state and method, state is an object that can have initial values, method that updates state by calling setState.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import './App.css';
 import MyComponent from './MyComponent';
 
 class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         isOn: false
+      };
+   }
+   toggle = () => {
+      this.setState({isOn: !this.state.isOn})
+   }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+           <h2>Welcome to React</h2>
+           <MyComponent name="Kyle"/>
         </header>
-        <h2>Welcome to React</h2>
-        <MyComponent name="Kyle"/>
+         <button onClick={this.toggle}>
+            {this.state.isOn ? 'On' : 'Off' }
+         </button>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
